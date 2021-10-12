8abf4cdd9e20e7e19287d4f1e3ce477b05185469
INOUE HIROTAKA <83050420+Aso2001032@users.noreply.github.com>
2021-10-12 10:04:28 +0900

    Create index.php

---
+++ PHPsourse/トップページ/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>トップページ</title>
+    <link rel="stylesheet" href="./css/sanitize.css">
+    <link rel="stylesheet" href="./css/all.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+<header>
+    <h1><a href="#">T　I　T　L　E</a></h1>
+    <nav class="menu">
+        <ul class="nav_menu1">
+            <li class="nav_menu1_item"><a href="" class="">新規会員登録</a></li>
+            <li class="nav_menu1_item"><a href="" class="">ご利用ガイド</a></li>
+            <li class="nav_menu1_item"><a href="" class="">お問い合わせ</a></li>
+            <li class="nav_menu1_item"><a href="" class="">サポート</a></li>
+        </ul>
+        <ul class="nav_menu2">
+            <li class="nav_menu2_item"><a href="" class=""><i class="fas fa-user-circle"></i> 会員情報</a></li>
+            <li class="nav_menu2_item"><a href="" class=""><i class="fas fa-heart"></i> お気に入り</a></li>
+            <li class="nav_menu2_item"><a href="" class=""><i class="fas fa-shopping-cart"></i> カート</a></li>
+            <li class="nav_menu2_item glass"><input type="text" name="sarch" class="sarch" value="何をお探しですか？"><button type="submit"><i class="fas fa-search"></i></button></li>
+        </ul>
+        <ul class="nav_menu3">
+            <li class="nav_menu3_item"><a href="" class="">デスクトップPC</a></li>
+            <li class="nav_menu3_item"><a href="" class="">ノートPC</a></li>
+            <li class="nav_menu3_item"><a href="" class="">ゲーミングPC</a></li>
+            <li class="nav_menu3_item"><a href="" class="">クリエイター向けPC</a></li>
+            <li class="nav_menu3_item"><a href="" class="">周辺機器</a></li>
+        </ul>
+    </nav>
+</header>
+<div class="cont">
+    <!-- ここからそれぞれのコード -->
+    aaaaaaaaaaaaaa
+</div>
+<footer>
+    <div class="foot-wrap">
+        <div class="menu-left">
+            <h3>menu-left</h3>
+            <ul class="foot-left">
+                <li><a href="#">footer-menu</a></li>
+                 <li><a href="#">footer-menu</a></li>
+                <li><a href="#">footer-menu</a></li>
+            </ul>
+        </div>
+        <div class="menu-center">
+            <h3>menu-center</h3>
+            <ul class="foot-center">
+                <li><a href="#">footer-menu</a></li>
+                <li><a href="#">footer-menu</a></li>
+                <li><a href="#">footer-menu</a></li>
+            </ul>
+        </div>
+        <div class="menu-right">
+            <h3>menu-right</h3>
+            <ul class="foot-right">
+                <li><a href="#">footer-menu</a></li>
+                <li><a href="#">footer-menu</a></li>
+                <li><a href="#">footer-menu</a></li>
+            </ul>
+        </div>
+        <small class="cmark">©2001システム開発3班
+        </small>
+    </div>
+</footer>
+</body>
+</html>
+
